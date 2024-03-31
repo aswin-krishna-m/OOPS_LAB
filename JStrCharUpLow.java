db8fdc7f62e00cec4d4c93ff56a9381b20ead769
aswin-krishna-m <aswinkrishnatm@gmail.com>
2024-03-31 09:33:54 +0530

    added String Handling Program Char Up Low

---
+++ JStrCharUpLow.java
@@ -0,0 +1,16 @@
+public class JStrCharUpLow {
+    public static void main(String[] args) {
+        String s1 = "Good Morning";
+        System.out.println("Actual String :" + s1+"\nTo Upper Case: "+s1.toUpperCase()+"\nTo Lower Case: "+s1.toLowerCase());
+    }
+}
+
+// **************OUTPUT**************
+
+/*
+
+Actual String :Good Morning
+To Upper Case: GOOD MORNING
+To Lower Case: good morning
+
+*/
